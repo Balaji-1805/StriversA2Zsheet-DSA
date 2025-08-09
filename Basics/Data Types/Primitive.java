b7eeb10167c2c099b55255be7de3541f529dbafd
Balaji-1805 <ugginabalaji2003@gmail.com>
2025-08-09 19:45:39 +0530

    Primitive.java Updated

--- Basics/Data Types/Primitive.java
+++ Basics/Data Types/Primitive.java
@@ -1,9 +1,9 @@
 //Primitive Data types:
 //byte short int long float double boolean char
+//Local variables cannot be accessed before initialization
 class Primitive 
 {
-    public static void main(String[] args) {
-        byte a;
+     byte a;
         short b;
         int c;
         long d;
@@ -11,7 +11,16 @@ class Primitive
         double f;
         boolean g;
         char h;
+    public static void main(String[] args) {
+       
         Primitive obj=new Primitive();
-        System.out.println(obj.a);
+        System.out.println("Default Value of byte:"+obj.a);//0
+        System.out.println("Default Value of short:"+obj.b);//0
+        System.out.println("Default Value of int:"+obj.c);//0
+        System.out.println("Default Value of long:"+obj.d);//0
+        System.out.println("Default Value of double:"+obj.e);//0.0
+        System.out.println("Default Value of float:"+obj.f);//0.0
+        System.out.println("Default Value of boolean:"+obj.g);//false
+        System.out.println("Default Value of char:"+obj.h);//null
     }
 }
